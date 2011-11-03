ddca66878cd27ab6fee0f7bfbdedce4168bdc614
chu <ecpplus@gmail.com>
2011-11-04 01:36:21 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterAlgo::Application.config.secret_token = '839226e4e595e88119ddeea2d97ecb4614ac3ee30679f7f7c2b90d656952e7c04fc677044b6d2770af45a5650f971b46d184f0fce5b75f10799c47ebf59a3a1d'
